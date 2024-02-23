d7f53a351661beda2f00b362f3d2b0931735eaea
Niki Dokovski <nickytd@gmail.com>
2024-02-23 09:15:37 +0100

    Reduce connection timeouts to the oidc provider in the init container

--- pkg/webhook/oidc-check.sh
+++ pkg/webhook/oidc-check.sh
@@ -2,8 +2,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-options="--connect-timeout 10 --max-time 10 --retry 3 --retry-delay 2"
-options="$options --retry-max-time 30 --cacert /etc/kube-rbac-proxy/ca.crt"
+options="--connect-timeout 2 --max-time 10 --retry 3 --retry-delay 1"
+options="$options --cacert /etc/kube-rbac-proxy/ca.crt"
 options="$options -w \"%{http_code}\" -sS -o /dev/null"
 check="curl ${options} "${OIDC_URL}/.well-known/openid-configuration""
 until  $( $check |grep -q "200" ); do
