8720e341d223c1153795589db526dfdc87e92bdc
Niki Dokovski <nikolai.dokovski@sap.com>
2025-05-13 16:01:30 +0200

    config: expose create ingress configuration

--- pkg/configuration/config.go
+++ pkg/configuration/config.go
@@ -1,16 +1,5 @@
-// Copyright 2024 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright 2025 SAP SE or an SAP affiliate company and Gardener contributors
+// SPDX-License-Identifier: Apache-2.0
 
 package configuration
 
@@ -119,39 +108,34 @@ func WithLog(l logr.Logger) Options {
 
 // CreateControllerConfigOrDie initializes the targets configurations or exits the controller when unsuccessful
 func CreateControllerConfigOrDie(path string, opts ...Options) *OIDCAppsControllerConfig {
-	var (
-		cf  []byte
-		err error
-	)
-
 	once.Do(func() {
 		config = &OIDCAppsControllerConfig{}
 		for _, o := range opts {
 			o(config)
 		}
 
-		if cf, err = os.ReadFile(filepath.Clean(path)); err != nil {
-			if config.log.IsZero() {
-				log.SetLogger(zap.New(zap.UseDevMode(true)))
-				config.log = log.Log.WithName("oidcAppsExtensionConfig")
-			}
-
-			config.log.Error(err, "failed to read extension configuration", "path", path)
-			panic("terminating")
+		cf, err := os.ReadFile(filepath.Clean(path))
+		if err != nil {
+			handleError(err, "failed to read extension configuration", path)
 		}
 
 		if err = yaml.Unmarshal(cf, config); err != nil {
-			if config.log.IsZero() {
-				log.SetLogger(zap.New(zap.UseDevMode(true)))
-				config.log = log.Log.WithName("oidcAppsExtensionConfig")
-			}
-
-			config.log.Error(err, "failed to unmarshal extension configuration")
-			panic("terminating")
+			handleError(err, "failed to unmarshal extension configuration", path)
 		}
 	})
 
 	return config
+}
+
+// handleError centralizes error handling logic
+func handleError(err error, message, path string) {
+	if config.log.IsZero() {
+		log.SetLogger(zap.New(zap.UseDevMode(true)))
+		config.log = log.Log.WithName("oidcAppsExtensionConfig")
+	}
+
+	config.log.Error(err, message, "path", path)
+	panic("terminating")
 }
 
 // GetOIDCAppsControllerConfig returns the loaded configuration
@@ -444,6 +428,16 @@ func (c *OIDCAppsControllerConfig) GetInsecureOidcSkipNonce(object client.Object
 	if c.Configuration.Oauth2Proxy != nil &&
 		c.Configuration.Oauth2Proxy.InsecureOidcSkipNonce != nil {
 		return ptr.Deref(c.Configuration.Oauth2Proxy.InsecureOidcSkipNonce, false)
+	}
+
+	return false
+}
+
+// ShallCreateIngress returns true if the target workload shall create an ingress
+func (c *OIDCAppsControllerConfig) ShallCreateIngress(object client.Object) bool {
+	t := c.fetchTarget(object)
+	if t.Ingress != nil {
+		return t.Ingress.Create
 	}
 
 	return false
